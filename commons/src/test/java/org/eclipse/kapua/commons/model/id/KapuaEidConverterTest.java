d394d63e3abd625df5b1093152910202898b7c25
VesnaGrumic <vesna.grumic@comtrade.com>
2020-06-02 13:36:29 +0200

    Removed unused parameter in JUnit test for KapuaEidConverter class
    Signed-off-by: VesnaGrumic <vesna.grumic@comtrade.com>

--- commons/src/test/java/org/eclipse/kapua/commons/model/id/KapuaEidConverterTest.java
+++ commons/src/test/java/org/eclipse/kapua/commons/model/id/KapuaEidConverterTest.java
@@ -18,10 +18,10 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Random;
 
 @Category(JUnitTests.class)
@@ -32,21 +32,15 @@ public class KapuaEidConverterTest extends Assert {
 
     private final BigInteger eid;
 
-    // bug in eclipse cannot convert this to a single parameter test
-    // https://github.com/junit-team/junit4/wiki/Parameterized-tests
-    private final long notUsed;
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> eids() {
-        return Arrays.asList(new Object[][]{
-                {new BigInteger(64, RANDOM), 0L},
-                {null, 0L},
-        });
+    @Parameters
+    public static Iterable<Object[]> eids() {
+        return Arrays.asList(
+                new Object[]{new BigInteger(64, RANDOM)},
+                new Object[] {null});
     }
 
-    public KapuaEidConverterTest(BigInteger eid, long notUsed) {
+    public KapuaEidConverterTest(BigInteger eid) {
         this.eid = eid;
-        this.notUsed = notUsed;
     }
 
     @Test
