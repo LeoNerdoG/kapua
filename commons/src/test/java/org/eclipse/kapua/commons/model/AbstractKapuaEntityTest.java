ce8f393a3342d66235e262b7cb7a2c393f53a699
VesnaGrumic <vesna.grumic@comtrade.com>
2020-06-08 12:28:30 +0200

    Added JUnit tests for AbstractKapuaEntity class
    Signed-off-by: VesnaGrumic <vesna.grumic@comtrade.com>

---
+++ commons/src/test/java/org/eclipse/kapua/commons/model/AbstractKapuaEntityTest.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.commons.model;
+
+import org.eclipse.kapua.commons.model.id.KapuaEid;
+import org.eclipse.kapua.model.KapuaEntity;
+import org.eclipse.kapua.model.id.KapuaId;
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+@Category(JUnitTests.class)
+public class AbstractKapuaEntityTest extends Assert {
+
+    private class ConcreteKapuaEntity extends AbstractKapuaEntity {
+
+        public ConcreteKapuaEntity(KapuaId scopeId) {
+            super(scopeId);
+        }
+
+        public ConcreteKapuaEntity(KapuaEntity entity) {
+            super(entity);
+        }
+
+        @Override
+        public String getType() {
+            return null;
+        }
+    }
+
+    @Test
+    public void copyConstructorTest() {
+        KapuaId scopeId = new KapuaEid();
+        AbstractKapuaEntity kapuaEntity = new ConcreteKapuaEntity(scopeId);
+        kapuaEntity.setId(new KapuaEid());
+        kapuaEntity.setCreatedBy(new KapuaEid());
+        kapuaEntity.setCreatedOn(new Date());
+        AbstractKapuaEntity kapuaCopyEntity = new ConcreteKapuaEntity(kapuaEntity);
+        assertEquals(" kapuaCopyEntity.getId()", kapuaEntity.getId(), kapuaCopyEntity.getId());
+        assertEquals("kapuaCopyEntity.getScopeId()", kapuaEntity.getScopeId(), kapuaCopyEntity.getScopeId());
+        assertEquals("kapuaCopyEntity.getCreatedBy()", kapuaEntity.getCreatedBy(), kapuaCopyEntity.getCreatedBy());
+        assertEquals("kapuaCopyEntity.getCreatedOn()", kapuaEntity.getCreatedOn(), kapuaCopyEntity.getCreatedOn());
+    }
+
+    @Test
+    public void prePersistsActionTest() {
+        BigInteger bigInteger = new BigInteger("12");
+        KapuaId scopeId = new KapuaEid(bigInteger);
+        AbstractKapuaEntity kapuaEntity = new ConcreteKapuaEntity(scopeId);
+        kapuaEntity.prePersistsAction();
+        AbstractKapuaEntity kapuaCopyEntity = new ConcreteKapuaEntity(kapuaEntity);
+        assertEquals("kapuaCopyEntity.getId()", kapuaEntity.getId(), kapuaCopyEntity.getId());
+        assertEquals("kapuaCopyEntity.getCreatedBy()", kapuaEntity.getCreatedBy(),kapuaCopyEntity.getCreatedBy());
+        assertEquals("kapuaCopyEntity.getCreatedOn()", kapuaEntity.getCreatedOn(), kapuaCopyEntity.getCreatedOn());
+    }
+}
