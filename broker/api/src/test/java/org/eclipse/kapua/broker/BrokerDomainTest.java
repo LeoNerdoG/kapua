5c3f7fb0bc4d7dedc8ab6e3f0ba3b78d4fa48090
zanpizmoht <zan.pizmoht@comtrade.com>
2020-08-17 14:55:25 +0200

    Added unit tests for broker/api module
    Signed-off-by: zanpizmoht <zan.pizmoht@comtrade.com>

---
+++ broker/api/src/test/java/org/eclipse/kapua/broker/BrokerDomainTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.broker;
+
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(JUnitTests.class)
+public class BrokerDomainTest extends Assert {
+
+    @Test
+    public void gettersTest() {
+        BrokerDomain brokerDomain = new BrokerDomain();
+        assertEquals("broker", brokerDomain.getName());
+        assertEquals("[connect]", brokerDomain.getActions().toString());
+        assertFalse(brokerDomain.getGroupable());
+    }
+
+}
