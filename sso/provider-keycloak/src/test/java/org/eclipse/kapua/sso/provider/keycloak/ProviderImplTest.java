fac3376106ed72855475be14dd4b7465e3c7cd4a
nezau-ct <neza.urbas@endava.com>
2020-11-12 15:21:30 +0100

    Sso provider all changes
    Signed-off-by: nezau-ct <neza.urbas@endava.com>

--- sso/provider-keycloak/src/test/java/org/eclipse/kapua/sso/provider/keycloak/ProviderImplTest.java
+++ sso/provider-keycloak/src/test/java/org/eclipse/kapua/sso/provider/keycloak/ProviderImplTest.java
@@ -1,4 +1,29 @@
 package org.eclipse.kapua.sso.provider.keycloak;
 
-public class ProviderImplTest {
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(JUnitTests.class)
+public class ProviderImplTest extends Assert {
+
+    ProviderImpl providerImpl;
+
+    @Before
+    public void setUp() {
+        providerImpl = new ProviderImpl();
+    }
+
+    @Test
+    public void getIdTest() {
+        assertEquals("keycloak", providerImpl.getId());
+    }
+
+    @Test
+    public void createLocatorTest() {
+        assertThat("Instance of KeycloakSingleSignOnLocator expected.", providerImpl.createLocator(), IsInstanceOf.instanceOf(KeycloakSingleSignOnLocator.class));
+    }
 }
