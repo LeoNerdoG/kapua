2c968aed5c7ff40e7d3d1be03f19626e2fa98472
nezau-ct <neza.urbas@endava.com>
2020-11-12 15:54:47 +0100

    Sso all changes
    Signed-off-by: nezau-ct <neza.urbas@endava.com>

--- sso/provider-keycloak/src/test/java/org/eclipse/kapua/sso/provider/keycloak/setting/KeycloakSsoSettingKeysTest.java
+++ sso/provider-keycloak/src/test/java/org/eclipse/kapua/sso/provider/keycloak/setting/KeycloakSsoSettingKeysTest.java
@@ -1,4 +1,24 @@
 package org.eclipse.kapua.sso.provider.keycloak.setting;
 
-public class KeycloakSsoSettingKeysTest {
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.eclipse.kapua.sso.provider.generic.setting.GenericSsoSettingKeys;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(JUnitTests.class)
+public class KeycloakSsoSettingKeysTest extends Assert {
+
+    @Test
+    public void keycloakUriTest() {
+        assertNotNull(KeycloakSsoSettingKeys.valueOf("KEYCLOAK_URI"));
+        assertEquals("Expected and actual values should be the same!", "sso.keycloak.uri", KeycloakSsoSettingKeys.KEYCLOAK_URI.key());
+    }
+
+    @Test
+    public void keycloakRealmTest() {
+        assertNotNull(KeycloakSsoSettingKeys.valueOf("KEYCLOAK_REALM"));
+        assertEquals("Expected and actual values should be the same!", "sso.keycloak.realm", KeycloakSsoSettingKeys.KEYCLOAK_REALM.key());
+    }
+//    UPORABI REFLECTIONZA CONSTRUCTOR
 }
--- sso/provider-keycloak/src/test/java/org/eclipse/kapua/sso/provider/keycloak/setting/KeycloakSsoSettingTest.java
+++ sso/provider-keycloak/src/test/java/org/eclipse/kapua/sso/provider/keycloak/setting/KeycloakSsoSettingTest.java
@@ -1,4 +1,22 @@
 package org.eclipse.kapua.sso.provider.keycloak.setting;
 
-public class KeycloakSsoSettingTest {
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Test;
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.junit.Assert;
+import org.junit.experimental.categories.Category;
+
+@Category(JUnitTests.class)
+public class KeycloakSsoSettingTest extends Assert{
+
+    @Test
+    public void constructorTest() {
+        KeycloakSsoSetting keycloakSsoSetting = new KeycloakSsoSetting("sso-keycloak-setting.properties");
+        assertThat("Instance of KeycloakSsoSetting expected.", keycloakSsoSetting, IsInstanceOf.instanceOf(KeycloakSsoSetting.class));
+    }
+
+    @Test
+    public void getInstanceTest() {
+        assertThat("Instance of KeycloakSsoSetting expected.", KeycloakSsoSetting.getInstance(), IsInstanceOf.instanceOf(KeycloakSsoSetting.class));
+    }
 }
