ea021eb7086fd27b32e08e3057984cf264acde37
nezau-ct <neza.urbas@endava.com>
2020-11-13 08:04:17 +0100

    Sso changes
    Signed-off-by: nezau-ct <neza.urbas@endava.com>

--- sso/provider/src/test/java/org/eclipse/kapua/sso/provider/internal/DisabledLocatorTest.java
+++ sso/provider/src/test/java/org/eclipse/kapua/sso/provider/internal/DisabledLocatorTest.java
@@ -12,6 +12,12 @@
 package org.eclipse.kapua.sso.provider.internal;
 
 import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.eclipse.kapua.sso.JwtProcessor;
+import org.eclipse.kapua.sso.SingleSignOnService;
+import org.eclipse.kapua.sso.exception.SsoAccessTokenException;
+import org.eclipse.kapua.sso.exception.uri.SsoUriException;
+import org.eclipse.kapua.sso.provider.SingleSignOnProvider;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,15 +25,34 @@ import org.junit.experimental.categories.Category;
 
 @Category(JUnitTests.class)
 public class DisabledLocatorTest extends Assert {
-    DisabledLocator disabledLocator;
+    SingleSignOnProvider.ProviderLocator disabledLocator;
 
     @Before
     public void setUp() {
-//        disabledLocator = new SingleSignOnProvider.ProviderLocator();
+        disabledLocator = DisabledLocator.INSTANCE;
     }
 
     @Test
-    public void isEnabledTest() {
-//        assertFalse();
+    public void getServiceTest() throws SsoUriException, SsoAccessTokenException {
+        assertThat(disabledLocator.getService(), IsInstanceOf.instanceOf(SingleSignOnService.class));
+        assertEquals(false, disabledLocator.getService().isEnabled());
+        assertNull("Null expected.", disabledLocator.getService().getLoginUri(null, null));
+        assertNull("Null expected.", disabledLocator.getService().getAccessToken(null, null));
+        assertNull("Null expected.", disabledLocator.getService().getLogoutUri(null, null, null));
+    }
+
+    //    this test method will be upgraded once method close() gets a body
+    @Test
+    public void getProcessorTest() throws Exception {
+        assertThat(disabledLocator.getProcessor(), IsInstanceOf.instanceOf(JwtProcessor.class));
+        assertFalse("False expected.", disabledLocator.getProcessor().validate(null));
+        assertNull("Null expected.", disabledLocator.getProcessor().process(null));
+        disabledLocator.getProcessor().close();
+    }
+
+    //    this test method will be upgraded once method close() gets a body
+    @Test
+    public void closeTest() throws Exception {
+        disabledLocator.close();
     }
 }
